37f33e04cd9bea6b3115de4769db3eb03a573b65
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2024-10-22 17:31:35 -0300

    nova linguagem

---
+++ src/boasvindas.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    // adicionar um pequeno cabeçalho
+    printf("---------------Programa Mensagem-------------");
+    printf("\nSeja Bem Vindo ao nosso programa\n");
+    printf("\n---------------------Fim----------------\n");
+
+    return 0; 
+
+}
---
+++ src/hello.c
@@ -0,0 +1,13 @@
+//  Importação da biblioteca padrão para o uso de teclado e monitor
+#include <stdio.h>
+
+
+
+int main () {
+//  para exibir uma mnesagem na tela iremos usar
+// o comando printf (exibir formatando)
+printf("Hello, World!\n");
+// informar a o compildor que o programa
+// acabou
+return 0;
+}
